da0c0296f8b5129d0bca0928bf4b263138aa2334
MAC <mac@MACdeMacBook-Air.local>
2022-09-13 08:41:20 +0800

    first commit

---
+++ pythonProject1/main.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+import torch
+from IPython import display
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+num_inputs = 2
+num_examples = 1000
+true_w = [2, -3.4]
+true_b = 4.2
+features = torch.randn(num_examples, num_inputs,
+                       dtype=torch.float32)
+labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()),
+                       dtype=torch.float32)
+plt.scatter(features[:, 1].numpy(), labels.numpy(),);
+plt.show()
